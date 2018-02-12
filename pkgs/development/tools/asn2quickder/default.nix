d72138ad20089eceb9921266e22543e304bdd848
Michiel Leenaars <ml.software@leenaa.rs>
2018-02-12 17:30:17 +0100

    asn2quickder: 0.7RC1 -> 1.2-6

--- pkgs/development/tools/asn2quickder/default.nix
+++ pkgs/development/tools/asn2quickder/default.nix
@@ -1,30 +1,21 @@
-{ stdenv, fetchFromGitHub, python2Packages, makeWrapper }:
+{ stdenv, fetchFromGitHub, python2Packages, makeWrapper, cmake }:
 
-stdenv.mkDerivation rec {
+python2Packages.buildPythonPackage rec {
   pname = "asn2quickder";
   name = "${pname}-${version}";
-  version = "0.7-RC1";
+  version = "1.2-6";
 
   src = fetchFromGitHub {
-    sha256 = "0ynajhbml28m4ipbj5mscjcv6g1a7frvxfimxh813rhgl0w3sgq8";
+    sha256 = "00wifjydgmqw2i5vmr049visc3shjqccgzqynkmmhkjhs86ghzr6";
     rev = "version-${version}";
     owner = "vanrein";
-    repo = "${pname}";
+    repo = "quick-der";
   };
 
-  propagatedBuildInputs = with python2Packages; [ pyparsing makeWrapper ];
-
-  patchPhase = with python2Packages; ''
-    substituteInPlace Makefile \
-      --replace '..' '..:$(DESTDIR)/${python.sitePackages}:${python2Packages.pyparsing}/${python.sitePackages}' \
-    '';
-
-  installPhase = ''
-    mkdir -p $out/${python2Packages.python.sitePackages}/
-    mkdir -p $out/bin $out/lib $out/sbin $out/man
-    make DESTDIR=$out PREFIX=/ all
-    make DESTDIR=$out PREFIX=/ install
-    '';
+  propagatedBuildInputs = with python2Packages; [
+    pyparsing makeWrapper cmake asn1ate pip pytestrunner pytest six
+    wheel
+  ];
 
   meta = with stdenv.lib; {
     description = "An ASN.1 compiler with a backend for Quick DER";
