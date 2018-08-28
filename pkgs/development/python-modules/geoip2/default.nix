7af879418360b70b47bddd96ab8e9ce1494eb107
Michiel Leenaars <ml.software@leenaa.rs>
2018-08-28 23:18:28 +0200

    pythonPackages.geoip2: init at 2.9.0

---
+++ pkgs/development/python-modules/geoip2/default.nix
@@ -0,0 +1,30 @@
+{ stdenv, python3Packages, libmaxminddb }:
+
+python3Packages.buildPythonPackage rec {
+  pname = "geoip2";
+  version = "2.9.0";
+  name = "${pname}-${version}";
+
+  src = python3Packages.fetchPypi {
+    inherit pname version;
+    sha256 = "1w7cay5q6zawjzivqbwz5cqx1qbdjw6kbriccb7l46p7b39fkzzp";
+  };
+
+  #patchPhase = ''
+  #  substituteInPlace requirements.txt --replace requests "#requests"
+  #'';
+
+  propagatedBuildInputs = with python3Packages;
+    [ python3Packages.requests
+      python3Packages.maxminddb
+      python3Packages.mock
+      python3Packages.requests-mock
+    ];
+
+  meta = with stdenv.lib; {
+    description = "Python geospatial data analysis framework";
+    homepage = http://www.maxmind.com/;
+    license = licenses.asl20;
+    maintainers = with maintainers; [ leenaars ];
+  };
+}
